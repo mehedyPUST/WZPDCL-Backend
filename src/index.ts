372fe4c9b38ecd57d3b657975bf050b7d067e548
mehedyPUST <mehedy.pust@gmail.com>
2026-07-15 23:18:34 +0600

    commented codeees

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
-// import app from './app';
-// import { connectDB } from './db';
-
-// const PORT = process.env.PORT || 5000;
-
-// connectDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server running on port ${PORT}`);
-//     });
-// });
-
-
 import app from './app';
 import { connectDB } from './db';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
